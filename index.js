01af1b8141c01b7179e097903edc1e4a389fb827
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-06-12 13:04:45 -0300

    chore: removing console.logs

--- index.js
+++ index.js
@@ -48,7 +48,6 @@ app.use(ExpressSession({
 // Bots fabrication
 //
 const speech = require(`./bot/speeches/${process.env.SPEECH_VERSION || 'v0'}`).speech
-console.log('SPEECH LOADED, VERSION: ', process.env.SPEECH_VERSION)
 const fabricated = new BotFactory(speech)
   .fabricate()
   .then(bots => {
@@ -64,7 +63,6 @@ const fabricated = new BotFactory(speech)
       // Set up pressure stuff
       //
       botSkills.pressure.fetchWidgets({ botData })
-      console.log('BOT CREATED')
       console.info(`Bot[${id}] exposed in endpoint: ${endpoint}`.blue)
     })
   })
